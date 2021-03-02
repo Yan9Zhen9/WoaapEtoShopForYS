2bb2902a9c3976bb83ade403a6dcc6e13bd864de
yibo.jiang <1525520375@qq.com>
2021-03-02 15:48:27 +0800

    去掉多余publishes

--- src/OrderforysServiceProvider.php
+++ src/OrderforysServiceProvider.php
@@ -14,31 +14,9 @@ class OrderforysServiceProvider extends ServiceProvider
      */
     public function boot(Router $router)
     {
-        $router->aliasMiddleware('orderforys', \Yan9\Orderforys\Middleware\OrderforysMiddleware::class);
-
         $this->publishes([
             __DIR__.'/Config/orderforys.php' => config_path('orderforys.php'),
         ], 'orderforys_config');
-
-        $this->loadRoutesFrom(__DIR__ . '/Routes/web.php');
-
-        $this->loadMigrationsFrom(__DIR__ . '/Migrations');
-
-        $this->loadTranslationsFrom(__DIR__ . '/Translations', 'orderforys');
-
-        $this->publishes([
-            __DIR__ . '/Translations' => resource_path('lang/vendor/orderforys'),
-        ]);
-
-        $this->loadViewsFrom(__DIR__ . '/Views', 'orderforys');
-
-        $this->publishes([
-            __DIR__ . '/Views' => resource_path('views/vendor/orderforys'),
-        ]);
-
-        $this->publishes([
-            __DIR__ . '/Assets' => public_path('vendor/orderforys'),
-        ], 'orderforys_assets');
 
         if ($this->app->runningInConsole()) {
             $this->commands([
